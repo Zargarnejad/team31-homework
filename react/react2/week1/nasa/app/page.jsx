1be28b638870bafeb5ef6fd176f9fdc649885e2d
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-06-26 18:58:54 +0200

    Make rover app responsive and create test data asset to use in development

--- react/react2/week1/nasa/app/page.jsx
+++ react/react2/week1/nasa/app/page.jsx
@@ -3,18 +3,20 @@ const API_KEY = "lvfBJ4fDBKiwaeLkZ6JvznJ1QB2DbLevZ4Nxevrg";
 const picOfTheDayURL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 import MarsRoverPhoto from "../components/MarsRoverPhoto/MarsRoverPhoto";
 import styles from "./page.module.css";
+// import data from "../assets/pod.json";
 
 export default async function HomePage() {
   const data = await fetch(picOfTheDayURL).then((response) => response.json());
 
   return (
     <>
-    <div className={styles.mainContainer}>
-      <p>{data.title}</p>
-      <img src={data.url} />
-      <p>{data.explanation}</p>
-    </div>
-    <MarsRoverPhoto/>
+      <div className={styles.mainContainer}>
+        <h1>Picture of The Day: {data.title}</h1>
+        <img src={data.url} className={styles.picOfTheDay} />
+        <p className={styles.picOfTheDayDescription}>{data.explanation}</p>
+        <h1>Rover Photo Of The Day</h1>
+        <MarsRoverPhoto />
+      </div>
     </>
   );
 }
--- react/react2/week1/nasa/components/MarsRoverPhoto/MarsRoverPhoto.jsx
+++ react/react2/week1/nasa/components/MarsRoverPhoto/MarsRoverPhoto.jsx
@@ -2,8 +2,9 @@
 
 import React, { useState, useEffect } from "react";
 import styles from "./MarsRoverPhoto.module.css";
+// import roverPhotoResponse from "../../assets/rover.json";
 
-const API_KEY = "8f077cfc-82b7-4e9f-8f50-95f72b62df62";
+const API_KEY = "lvfBJ4fDBKiwaeLkZ6JvznJ1QB2DbLevZ4Nxevrg";
 const marsRoverPhotoAPI = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${API_KEY}`;
 
 export default function MarsRoverPhoto() {
@@ -13,6 +14,7 @@ export default function MarsRoverPhoto() {
       const roverPhotoResponse = await fetch(marsRoverPhotoAPI).then(
         (response) => response.json()
       );
+
       setRoverPhoto(roverPhotoResponse);
     };
 
@@ -24,10 +26,10 @@ export default function MarsRoverPhoto() {
       {roverPhoto?.photos?.length ? (
         <div className={styles.roverContainer}>
           {roverPhoto.photos.map((photo, index) => (
-            <div key={index} className={styles.roverImage}>
+            <div key={index}>
               <p>Date {photo.earth_date}</p>
               <img
-                className={styles.nasaPicOfTheDayImg}
+                className={styles.roverImage}
                 src={photo.img_src}
                 alt={photo.roverName}
               />
